315510c2d74fa49a55a1eda018e58a1a928cb85e
Jaharim <jaharim1993@gmail.com>
2021-11-06 02:23:28 +0900

    Create Add Indie's Modal, Backdrop mechanism

--- src/admin/pages/Admin.js
+++ src/admin/pages/Admin.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Backdrop from "../../shared/components/UIElements/Backdrop";
 import AddIndie from "../components/AddIndie";
 
 const Admin = (props) => {
+  const [backdropStatus, setBackdropStatus] = useState(false);
+
+  const addIndieModalOpenHandler = (event) => {
+    event.preventDefault();
+    setBackdropStatus(true);
+  };
+
+  const addIndieModalCloseHandler = (event) => {
+    event.preventDefault();
+    setBackdropStatus(false);
+  };
+
   const addIndieHandler = (event) => {
     console.log("good");
   };
 
   return (
     <div>
-      <AddIndie onSubmit={addIndieHandler} />
+      <div>
+        <div onClick={addIndieModalOpenHandler}>Add Indie</div>
+        {backdropStatus && <Backdrop onClick={addIndieModalCloseHandler} />}
+        {backdropStatus && (
+          <AddIndie className="addIndie-modal" onSubmit={addIndieHandler} />
+        )}
+      </div>
       <div>여기는 Edit Indie</div>
       <div>여기는 Delete Indie</div>
     </div>
